17a36d4c7e72aec28104f8a6f037b4f127961127
takuro.katsube <takuroktb@gmail.com>
2021-07-20 06:45:19 +0000

    user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
-    include Pagy::Backend
+   before_action :configure_permitted_parameters, if: :devise_controller? 
+   
+   include SessionsHelper
+   include Pagy::Backend
+    
+   private
+
+   def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
+   end
+   
+   def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up,keys:[:email])
+    end
 end
