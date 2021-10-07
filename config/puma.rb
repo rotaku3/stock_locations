5054a3ea5087a263690f3751fe5ae487f0014516
takuro.katsube <takuroktb@gmail.com>
2021-10-07 09:17:25 +0000

    puma.rb deploy.rb

--- config/puma.rb
+++ config/puma.rb
@@ -16,7 +16,11 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
 #port ENV.fetch("PORT") { 3000 }
-bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
+app_dir = File.expand_path("../..", __FILE__)
+bind "unix://#{app_dir}/tmp/sockets/puma.sock"
+pidfile "#{app_dir}/tmp/pids/puma.pid"
+state_path "#{app_dir}/tmp/pids/puma.state"
+stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
 
 # Specifies the `environment` that Puma will run in.
 #
